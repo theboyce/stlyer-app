dfe8836e3b9e38e2d78d273cd5e5ba7542cd504c
theboyce <kwabenaadofo330@gmail.com>
2023-05-03 18:04:33 -1200

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const box = document.querySelector('div'),
+      input = document.querySelector('input');
+
+
+input.addEventListener('input', () => {
+    box.style.borderRadius = input.value;
+    box.style.background = input.value;
+} );
